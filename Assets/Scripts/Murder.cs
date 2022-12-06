e81b107e954c8e8de89f56081cceb2d5e8a05e91
Marcelo-Teixeira-Barbosa <marcelo.barbosa1404@gmail.com>
2022-12-06 14:31:43 -0300

    atualização do mapa

--- Assets/Scripts/Murder.cs
+++ Assets/Scripts/Murder.cs
@@ -2,25 +2,37 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.SceneManagement;
+
 public class Murder : MonoBehaviour
 {
     private int[,] graph
-                = new int[,] {
-                            { 0,2,0,0,0,0,0,0,0,0,0 },
-                            { 2,0,8,0,3,0,0,0,0,0,0 },
-                            { 0,8,0,0,0,0,3,0,0,0,0 },
-                            { 0,0,0,0,1,0,0,0,0,0,0 },
-                            { 0,3,0,1,0,4,0,6,0,0,0 },
-                            { 0,0,0,0,4,0,4,0,6,0,0 },
-                            { 0,0,3,0,0,4,0,0,0,0,7 },
-                            { 0,0,0,0,6,0,0,0,4,0,0 },
-                            { 0,0,0,0,0,6,0,4,0,1,0 },
-                            { 0,0,0,0,0,0,0,0,1,0,4 },
-                            { 0,0,0,0,0,0,7,0,0,4,0 }
+<<<<<<< HEAD
+           = new int[,] {
+=======
+            = new int[,] {
+>>>>>>> 7e682520b3b3c1ae560250a14605e43596f28aec
+                            { 0,2,0,0,0,0,0,0,0,0,0,0,0,0 },
+                            { 2,0,8,0,3,0,0,0,0,0,0,0,0,0 },
+                            { 0,8,0,0,0,0,3,0,0,0,0,0,0,0 },
+                            { 0,0,0,0,1,0,0,0,0,0,0,0,0,0 },
+                            { 0,0,0,1,0,4,0,6,0,0,0,0,0,0 },
+                            { 0,0,0,0,4,0,4,0,6,0,0,0,0,0 },
+                            { 0,0,3,0,0,4,0,0,0,0,0,4,0,5 },
+                            { 0,0,0,0,6,0,0,0,4,0,0,0,0,0 },
+                            { 0,0,0,0,0,6,0,4,0,1,0,0,0,0 },
+                            { 0,0,0,0,0,6,0,0,0,1,0,0,0,0 },
+                            { 0,0,0,0,0,0,0,0,0,4,0,3,0,0 },
+                            { 0,0,0,0,0,0,4,0,0,0,3,0,5,0 },
+                            { 0,0,0,0,0,0,0,0,0,0,0,5,0,4 },
+                            { 0,0,0,0,0,0,5,0,0,0,0,0,4,0 },
                             };
+<<<<<<< HEAD
+                       };
     private Vector3[] unityGraphPosition
+=======
+                        };
+    private Vector3[] unityGraphPosition 
+>>>>>>> 7e682520b3b3c1ae560250a14605e43596f28aec
         = { new Vector3(-175.525f,-637.58f,-91.562f), //A
             new Vector3(-179.74f,-637.58f,-91.562f), //B
             new Vector3(-195.61f,-637.58f,-91.562f), //C
@@ -32,20 +44,11 @@ public class Murder : MonoBehaviour
             new Vector3(-187.58f,-637.58f,-73.56f), //I
             new Vector3(-187.58f,-637.58f,-71.56f), //J
             new Vector3(-195.6f,-637.58f,-71.56f), //K
+            new Vector3(-195.56f,-638.14f,-77.62f), //L
+            new Vector3(-205.47f,-638.14f,-77.62f), //M
+            new Vector3(-205.47f,-638.14f,-85.52f), //N
         };
-    private string[] frasesGame = {
-            "Porque voc� est� desistindo de novo?", //A
-            "Quando vai perceber que voc� cavou a pr�pria cova?", //B
-            "N�o tem fim, e voc� sabia.", //C
-            "Sabemos o que voc� fez.", //D
-            "Estaremos sempre lhe observando.", //E
-            "Est� satisfeito agora?", //F
-            "Voc� causou tudo isso.", //G
-            "Agora resolva.", //H
-            "Ele j� sabe, nois sabemos.", //I
-            "Voc� podia ter ajudado quando podia.", //J
-            "Agora � tarde.", //K
-        };
+
     public int playerLocation;
     public GameObject player;
     public int murderLocation;
@@ -58,19 +61,6 @@ public class Murder : MonoBehaviour
     public ArrayList fullpath;
     [Range(0f, 1f)]
     public float t;
-
-    //HUD
-    public Text playerPositionText;
-    public Text murderShortestPath;
-    public Text shortestPathText;
-
-    //Audio
-    public AudioSource laughSong;
-    public AudioSource cansadoSong;
-
-    //Dialog
-    public Text dialogInformationText;
-    
     private void Awake()
     {
         this.transform.position = unityGraphPosition[murderLocation];
@@ -82,80 +72,33 @@ public class Murder : MonoBehaviour
     }
     private void Update()
     {
-
         transform.LookAt(player.transform);
         playerLocation = player.GetComponent<Linus>().playerPosition;
         rb.position = Vector3.Lerp(this.transform.position, pathToGo, t);
-
-
-        playerPositionText.text = playerLocation.ToString();
     }
     IEnumerator FindPlayerCorroutine()
     {
         while (!playerCaptured || murderLocation != playerLocation)
         {
-            // Debug.Log("Calculando nova trajetoria!");
-
-            laughSong.Play();
-
+            Debug.Log("Calculando nova trajetoria!");
             ArrayList fullpath = ShortestPath.Dijkstra(graph, murderLocation, playerLocation);
-            string text = "";
-            foreach (int path in fullpath)
-                text += "->" + path;
-
-            float tempoDeRecalculo = 0;
-            for (int i = 0; i < fullpath.Count - 1;i++) {
-                int origem = (int)fullpath[i];
-                int destino = (int)fullpath[i + 1];
-                tempoDeRecalculo += graph[origem, destino];
-            }
-            shortestPathText.text = tempoDeRecalculo.ToString();
-            murderShortestPath.text = text;
             StartCoroutine(movementMurderOnebyOne(fullpath));
-
-            float tempoTotal = 25.0f + tempoDeRecalculo;
-
-            // Debug.Log("Tempo para recalcular: " + tempoTotal);
-            
-            yield return new WaitForSeconds(tempoTotal);
+            yield return new WaitForSeconds(60.0f);
         }
     }
     IEnumerator movementMurderOnebyOne(ArrayList fullpath){
-        int origem = -1;
-        int destino = -1;
-        float peso = 1f;
-        for (int i = 0; i < fullpath.Count - 1; i++)
+        foreach(int path in fullpath){
+            Debug.Log(path);
+        }
+        foreach (int path in fullpath)
         {
-            origem = (int)fullpath[i];
-            destino = (int)fullpath[i + 1];
-            peso = graph[origem, destino];
-
-            // Debug.Log("Estou no vertice: "+ origem);
-            // Debug.Log("Indo ao vertice: " + destino);
-            // Debug.Log("Com o Peso: " + peso);
-
-            pathToGo = unityGraphPosition[destino];
-            murderLocation = destino;
-
-            cansadoSong.pitch -= peso * 0.01f;
-            cansadoSong.Play();
-
-            this.dialogInformationText.text = frasesGame[i];
-
-            //Chegando l�, eu descanso o tanto que andei
-            yield return new WaitForSeconds(1f + peso);
-        }
-        this.dialogInformationText.text = "";
-    }
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player")) {
-            // Debug.Log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-            SceneManager.LoadScene("GameOver");
+            Debug.Log("Estou no vertice: " + path);
+            pathToGo = unityGraphPosition[path];
+            murderLocation = path;
+            // float peso = graph[path, path + 1];
+            //t = 0.1f - (peso / 100f);
+            //t = 0.3f;
+            yield return new WaitForSeconds(6f);
         }
     }
-
-
-
-
 }
